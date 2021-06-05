da1888836fb9a01da58d37c2377ff0ebbc14f581
Jocelyn Hsu <jocelynhsu8@gmail.com>
2021-06-05 16:07:46 -0700

    changed calculation to be flexible with img dim

--- utils.py
+++ utils.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 def content_loss(content_img, gen_img):
-    """ Calculate mean squared error between content image and generated image.
+    """ Calculate mean squared error between activations of content image and generated image.
 
     Args:
-        contentimg (np array): original input image
-        gen_img (np array): [description]
+        contentimg (np array): content image
+        gen_img (np array): generated image
 
     Returns:
-        int: Calculated mean squared error
+        int: Computed mean squared error of activation
     """
     if content_img.shape != gen_img.shape:
         print('Images have different dimensions')
@@ -16,10 +16,10 @@ def content_loss(content_img, gen_img):
     sum = 0
     for i in range(1, content_img.shape[1] + 1):
         for j in range(1, content_img.shape[2] + 1):
-            for k in range(1, 4):
+            for k in range(1, content_img.shape[3] + 1):
                 sum += (content_img[1][i][j][k] - gen_img[1][i][j][k]) ** 2
             
-    sum /= content_img.shape[1] * content_img.shape[2] * 3
+    sum /= content_img.shape[1] * content_img.shape[2] * content_img.shape[3]
     return sum
 
 def main():
