96ea3a5bd6577cacddf483e93e7b2eb5e9f50046
Jocelyn Hsu <jocelynhsu8@gmail.com>
2021-06-05 23:19:05 -0700

    calculate style loss

--- utils.py
+++ utils.py
@@ -34,6 +34,52 @@ def calc_gram(filter):
     filter_t = np.transpose(filter)
     return np.matmul(filter, filter_t)
 
+def style_loss_ind(style_gram_mat, gen_gram_mat, weight): 
+    """ Calculate mean squared error of individual filter
+
+    Args:
+        style_img (np array): Gram matrix of style image
+        gen_img (np array): Gram matrix of generated image
+        weight (double): weight of layer
+
+    Returns:
+        [int]: weighted mean squared error of filter
+    """
+    if style_gram_mat.shape != gen_gram_mat.shape:
+        print('Images have different dimensions')
+        exit()
+    error = 0
+    for i in range(1, style_gram_mat.shape[1] + 1):
+        for j in range(1, style_gram_mat.shape[2] + 1):
+            for k in range(1, style_gram_mat.shape[3] + 1):
+                error += (style_gram_mat[1][i][j][k] - gen_gram_mat[1][i][j][k]) ** 2
+            
+    error /= style_gram_mat.shape[1] * style_gram_mat.shape[2] * style_gram_mat.shape[3]
+    return error * weight
+
+def style_loss_overall(style_img, gen_img, num_layers = 5, weight = []):
+    """ Calculate overall weighted mean squared error of all layers
+
+    Args:
+        style_img (np array): Style image
+        gen_img (np array): Generated image
+        num_layers (int, optional): Number of layers. Defaults to 5.
+        weight (list, optional): List of weights for each layer. Defaults to [].
+
+    Returns:
+        [int]: Overall weighted mean squared error of style image
+    """
+    if len(weight) == 0: # equal weight of each layer if weight not specified
+        for i in range(1,num_layers + 1):
+            weight.append(1 / num_layers)
+    error = 0
+    style_gram_mat = calc_gram(style_img)
+    gen_gram_mat = calc_gram(gen_img)
+    for i in range(1, num_layers + 1):
+        error += style_loss_ind(style_gram_mat, gen_gram_mat, weight[i - 1]) # create output image?
+    return error
+    
+
 
 def main():
     print('hello world')
