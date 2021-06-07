Fixed tape gradient not computing multiple gradients

neural-art.py
@@ -98,10 +98,16 @@ def training_step(image, optimizer, total_loss):
         total_loss (?): total loss (style and content)
         optimizer (tf.optimizers): TF Optimizer (Adam)
     """
-    tape = tf.GradientTape()
-    grad = tape.gradient(total_loss, image)
-    optimizer.apply_gradients([(grad, image)])
-    image.assign(bound_values(image))
+
+    print('Image: ', image)
+    print('Loss: ', total_loss)
+    with tf.GradientTape() as tape:
+        grad = tape.gradient(total_loss, image)
+
+        assert(grad is not None)
+
+        optimizer.apply_gradients([(grad, image)])
+        image.assign(bound_values(image))
 
 def bound_values(image):
     return tf.clip_by_value(image, clip_value_min = 0.0, clip_value_max = 1.0)
@@ -123,7 +129,7 @@ def generate(input_image, style_image, iterations = 200):
     mod_style = np.expand_dims(style_image, axis = 0)
 
     # Let generated_image be replica of input to begin with (May later change to noise)
-    mod_gen = mod_input
+    mod_gen = tf.Variable(mod_input)
    
     # Store content & style layers
     c_layer = 'block4_conv2'

utils.py
@@ -58,7 +58,7 @@ def style_loss_ind(style_img, gen_img, weight):
         print('Images have different dimensions')
         exit()
     error = tf.add_n([tf.reduce_mean((style_gram_mat - gen_gram_mat) ** 2)])    
-    error /= style_gram_mat.shape[1] * style_gram_mat.shape[2] * style_gram_mat.shape[3]
+    error /= (style_gram_mat.shape[1] * style_gram_mat.shape[2] * style_gram_mat.shape[3])
     return error * weight
 
 def style_loss_overall(style_img, gen_img, weight = []):
@@ -81,6 +81,9 @@ def style_loss_overall(style_img, gen_img, weight = []):
         style_gram_mat = calc_gram(style_img[i])
         gen_gram_mat = calc_gram(gen_img[i])
         error += style_loss_ind(style_gram_mat, gen_gram_mat, weight[i])
+        print('Style loss: ', error)
+
+    print('Style loss: ', error)
     return error
     
 def total_loss(content_img, content_gen_img, style_img_list, style_gen_img_list, alpha = 0.5, beta = 0.5, weight = []):