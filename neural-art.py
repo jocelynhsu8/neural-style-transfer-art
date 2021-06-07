Fixed gradient as None bug

neural-art.py
@@ -133,33 +133,10 @@ def intermediate_layers(layer_names):
 
     return tf.keras.Model([model.input], outputs)
 
-@tf.function()
-def training_step(image, optimizer, total_loss):
-    """ Performs one step of gradient descent to optimize image
-    
-    Args:
-        image (tf.Variable): generated image to optimize
-        total_loss (tf.Tensor): total loss (style and content)
-        optimizer (tf.optimizers): TF Optimizer (Adam)
-    """
-
-    print('Image: ', image)
-    print('Loss: ', total_loss)
-    with tf.GradientTape() as tape:
-        tape.watch(image)
-        grad = tape.gradient(total_loss, image)
-
-        print(grad)
-
-        assert(grad is not None)
-
-        optimizer.apply_gradients([(grad, image)])
-        image.assign(bound_values(image))
-
 def bound_values(image):
     return tf.clip_by_value(image, clip_value_min = 0.0, clip_value_max = 1.0)
 
-def generate(input_image, style_image, iterations = 200):
+def generate(input_image, style_image, iterations = 11):
     """ Generates resulting image through series of optimizations
 
     Args:
@@ -201,27 +178,31 @@ def generate(input_image, style_image, iterations = 200):
     style_model = intermediate_layers(s_layers)
     content_model = intermediate_layers([c_layer])
     
-    # Store content & style activations for inputs
-    c_activ = content_model(mod_input)
-    s_activ = style_model(mod_style)
-
     # Optimization loop
     for x in range(iterations):
-        print('Step: ', x)
-
-        # Preprocess generated image
-        mod_gen = gen_img * 255
-        mod_gen = tf.keras.applications.vgg19.preprocess_input(mod_gen)
-
-        # Compute  loss
-        g_c_activ = content_model(mod_gen)
-        g_s_activ = style_model(mod_gen)
-
-        total_loss = utils.total_loss(c_activ, g_c_activ, s_activ, g_s_activ)
-
-        # Update generated image
-        training_step(gen_img, optimizer, total_loss)
         
+        with tf.GradientTape() as tape:
+            # Preprocess generated image
+            mod_gen = gen_img * 255
+            mod_gen = tf.keras.applications.vgg19.preprocess_input(mod_gen)
+            
+            # Extract activations
+            c_activ = content_model(mod_input)
+            g_c_activ = content_model(mod_gen)
+            s_activ = style_model(mod_style)
+            g_s_activ = style_model(mod_gen)
+            
+            # Compute loss
+            total_loss = utils.total_loss(c_activ, g_c_activ, s_activ, g_s_activ)
+            
+            # Update generated image through backpropagation
+            grad = tape.gradient(total_loss, gen_img)
+
+            assert(grad is not None)
+
+            optimizer.apply_gradients([(grad, gen_img)])
+            gen_img.assign(bound_values(gen_img))
+
         # Print every 10 iterations to track progress
         if x % 10 == 0:
             print('Iteration #: ', x)

utils.py
@@ -10,21 +10,9 @@ def content_loss(content_img, gen_img):
     Returns:
         int: Computed mean squared error of activation
 
-    if content_img.shape != gen_img.shape:
-        print('Images have different dimensions')
-        exit()
-    error = 0
-    for i in range(0, content_img.shape[1]):
-        print('i : ', i, ' out of: ', content_img.shape[1])
-        for j in range(0, content_img.shape[2]):
-            for k in range(0, content_img.shape[3]):
-                error += (content_img[0][i][j][k] - gen_img[0][i][j][k]) ** 2
-            
-    error /= content_img.shape[1] * content_img.shape[2] * content_img.shape[3]
     """
     
     error = tf.add_n([tf.reduce_mean((content_img - gen_img) ** 2)])
-    print('Computed content_loss: ', error)
     return error
 
 def calc_gram(filter):
@@ -80,16 +68,15 @@ def style_loss_overall(style_img, gen_img, weight = []):
     if len(weight) == 0: # equal weight of each layer if weight not specified
         for i in range(0,num_layers):
             weight.append(1 / num_layers)
-
+    
     error = 0
+    
     for i in range(0, num_layers):
         error += style_loss_ind(style_img[i], gen_img[i], weight[i])
-        print('Style loss: ', error)
-    
     error /= num_layers
 
     assert(not tf.math.is_inf(error))
-
+    
     return error
     
 def total_loss(content_img, content_gen_img, style_img_list, style_gen_img_list, alpha = 0.5, beta = 0.5, weight = []):